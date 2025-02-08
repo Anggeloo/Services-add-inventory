ed265f589daf43a9b10265cedfba93fa0a65ed3b
Anggelo <anstalyn1999@gmail.com>
2025-02-07 23:00:15 -0500

    feat: :coffin: Elimination of conflicts

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-jdk-slim
-ARG JAR_FILE="target/app_microservices_add_inventory-0.0.1.jar"
-COPY ${JAR_FILE} app_iventory_add.jar
-EXPOSE 8081
-ENTRYPOINT ["java","-jar","app_iventory_add.jar"]
--- src/main/java/com/addiventory/services_add_inventory/Repository/InventoryAddResitory.java
+++
@@ -1,8 +0,0 @@
-package com.addiventory.services_add_inventory.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.addiventory.services_add_inventory.Models.Inventory;
-
-public interface InventoryAddResitory extends JpaRepository<Inventory, Integer>{
-    
-}
